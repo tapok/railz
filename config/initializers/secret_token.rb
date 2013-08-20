ff870a5adad5a8c54bdb42617876fa3a8709bd77
Gorbov Alexey <mender@tesla>
2013-08-20 16:54:25 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railz::Application.config.secret_key_base = '8ac5aefc4a23b53f113801ee2a1cb174c1defcf1dce0086702f547d5a2da6f71923accd0b14e00cdc042c6bdc05f2f91c00ea55d78bc325dd9edc55c42bbfb69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railz::Application.config.session_store :cookie_store, key: '_railz_session'
